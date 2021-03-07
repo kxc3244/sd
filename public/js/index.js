766245df6501f86e8ef853868e3d3ebd5d7f5f38
kxc3244 <48780213+kxc3244@users.noreply.github.com>
2021-03-07 03:30:26 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -81,9 +81,9 @@ if (!isBackspacing) {
   } else {
 
     isBackspacing = false;
-//     i = 0;
+    i = 0;
     isParagraph = false;
-    a = (a + 1) % ar.length; //Moves to next position in array, always looping back to 0
+    a = (a + 1); //% ar.length; //Moves to next position in array, always looping back to 0
     setTimeout(function(){ typeWriter(id, ar); }, 50);
 
   }
